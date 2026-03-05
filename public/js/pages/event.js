260023d9cdd259b4564550f7f9dd6766165e3e52
SudoWatson <aulennert@protonmail.com>
2026-03-05 00:16:18 -0500

    Just so I don't lose if needed

--- public/js/pages/event.js
+++ public/js/pages/event.js
@@ -120,27 +120,34 @@ timely.define("domReady", [], function() {
     };
 }), timely.define("scripts/event", ["jquery_timely", "domReady", "ai1ec_config", "scripts/event/osm_helper"], function(e, t, n, r) {
     var i = function() {
-            e("#osec-osm-canvas").length > 0 && timely.require(["libs/osm"], function(e) {
-                console.log("Required success");
-                console.log(r);
-                e(r.init_leaflet)
-            });
-        },
-        s = function() {
-            e(".ai1ec-gmap-placeholder:first").click(r.handle_show_map_when_clicking_on_placeholder)
-        },
-        o = function() {
-            e("#timely-description img[data-ai1ec-hidden]").each(function() {
-                var t = e(this),
-                    n = e("#timely-event-poster img").attr("src");
-                t.attr("src") != n && t.removeAttr("data-ai1ec-hidden")
-            })
-        },
-        u = function() {
-            t(function() {
-                i(), s(), o(), e(document).trigger("event_page_ready.ai1ec"), e("body").addClass("ai1ec-event-details-ready")
-            })
-        };
+        /* GMaps * /
+        e("#osec-gmap-canvas").length > 0 && timely.require(["libs/gmaps"], function(e) {
+            e(r.init_gmaps)
+        })
+        //*/
+        /* OSM */
+        e("#osec-osm-canvas").length > 0 && timely.require(["libs/osm"], function(e) {
+            console.log("Required success");
+            console.log(r);
+            e(r.init_leaflet)
+        });
+        //*/
+    },
+    s = function() {
+        e(".ai1ec-gmap-placeholder:first").click(r.handle_show_map_when_clicking_on_placeholder)
+    },
+    o = function() {
+        e("#timely-description img[data-ai1ec-hidden]").each(function() {
+            var t = e(this),
+            n = e("#timely-event-poster img").attr("src");
+            t.attr("src") != n && t.removeAttr("data-ai1ec-hidden")
+        })
+    },
+    u = function() {
+        t(function() {
+            i(), s(), o(), e(document).trigger("event_page_ready.ai1ec"), e("body").addClass("ai1ec-event-details-ready")
+        })
+    };
     return {
         start: u
     }
